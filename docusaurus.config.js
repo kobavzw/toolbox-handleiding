f720027eae7997c9c701cf33fa1eea0ab0bcfcf0
Wouter Henderickx <wouter.henderickx@gmail.com>
2023-03-31 08:53:59 +0200

    Inschrijvingen wijzigen

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,6 +96,12 @@ const config = {
         };
       },
       require.resolve('docusaurus-plugin-image-zoom'),
+      [
+        require.resolve("@cmfcmf/docusaurus-search-local"),
+        {
+          indexBlog: false,
+        },
+      ],
     ],
     stylesheets: [
       'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,200;0,400;0,500;0,600;0,800;1,400&display=swap'
