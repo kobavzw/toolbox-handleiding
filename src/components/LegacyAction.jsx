d6796b03ecfb8c281ad9acc941c82c7da25200f2
Wouter Henderickx <wouter.henderickx@gmail.com>
2023-03-31 09:53:20 +0200

    Make LegacyAction contain text only

--- src/components/LegacyAction.jsx
+++ src/components/LegacyAction.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function LegacyAction({ img, text = null }) {
+export default function LegacyAction({ img = null, text = null }) {
   return (
-    <div className={`inline-flex rounded-md align-middle items-center px-1 py-1/2 gap-1 ${text !== null && 'bg-white ring-1 ring-gray-300 '}`}>
-      <img
+    <div className={`${img === null ? 'py-1 px-2' : 'py-1/2 px-1'} inline-flex rounded-md align-middle items-center gap-1 ${text !== null && 'bg-white ring-1 ring-gray-300 '}`}>
+      {img !== null && <img
         src={require("@site/static/img/legacy_actions/" + img).default}
         className="max-h-[25px]"
-      />
+      />}
       {text !== null && <span className="text-indigo-700">{text}</span>}
     </div>
   );
