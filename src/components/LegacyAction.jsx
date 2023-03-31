6f1431f15a0f5ee010ddd6f92880bf65c5e15599
Wouter Henderickx <wouter.henderickx@gmail.com>
2023-03-31 14:23:52 +0200

    Fix LegacyAction vertical alignment

--- src/components/LegacyAction.jsx
+++ src/components/LegacyAction.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function LegacyAction({ img = null, text = null }) {
   return (
-    <p className={`${img === null ? 'py-1 px-2' : 'py-1/2 px-1'} inline-flex rounded-md align-middle items-center gap-1 ${text !== null && 'bg-white ring-1 ring-gray-300 '}`}>
+    <p className={`${img === null ? 'py-1 px-2' : 'py-1/2 px-1'} m-0 inline-flex rounded-md align-middle items-center gap-1 ${text !== null && 'bg-white ring-1 ring-gray-300 '}`}>
       {img !== null && <img
         src={require("@site/static/img/legacy_actions/" + img).default}
         className="max-h-[25px]"
